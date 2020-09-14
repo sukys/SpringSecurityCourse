270479e36a991077adb27a225dcc20aca5127bf9
Luis Sukys <sukys22@gmail.com>
2020-09-14 18:29:46 -0300

    Configurando usando a 'fluent configuration'.

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package guru.sfg.brewery.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,37 +19,44 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authorize -> {
-                authorize
-                .antMatchers("/", "/webjars/**", "/resources/**", "/login").permitAll()
-                .antMatchers("/beers*", "/beers/*", "/beers/find").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
-                .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll();
-            })
-            .authorizeRequests()
-            .anyRequest().authenticated()
-            .and()
-            .formLogin().and()
-            .httpBasic();
+            authorize
+                    .antMatchers("/", "/webjars/**", "/resources/**", "/login").permitAll()
+                    .antMatchers("/beers*", "/beers/*", "/beers/find").permitAll()
+                    .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
+                    .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll();
+        })
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().and()
+                .httpBasic();
     }
 
-
-    @Bean
     @Override
-    protected UserDetailsService userDetailsService() {
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("spring")
-                .password("guru")
-                .roles("ADMIN")
-                .build();
-
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("spring").password("{noop}guru").roles("ADMIN")
+                .and()
+                .withUser("user").password("{noop}password").roles("USER");
     }
+
+//    @Bean
+//    @Override
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("spring")
+//                .password("guru")
+//                .roles("ADMIN")
+//                .build();
+//
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("password")
+//                .roles("USER")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
 
 
 }
